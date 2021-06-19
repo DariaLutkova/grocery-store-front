cd7cb1545af9fa56f26dc829901c66b1dc3c8d9a
GlamDaria <daryalutkova@yandex.ru>
2021-06-19 22:53:56 +0300

    Add router structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Layout, Menu } from 'antd';
 
-function App() {
+import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout;
+
+export default function App() {
+  const isAuth = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" selectedKeys={[window.location.pathname]}>
+            {renderHeader()}
+          </Menu>
+        </Header>
+        <Content style={{ height: '100vh' }}>
+          {renderRoutes()}
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Darya Lutkova ©2021</Footer>
+      </Layout>
+    </Router>
   );
-}
 
-export default App;
+  function renderHeader() {
+    if (isAuth) return (
+      <>
+        <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
+        <Menu.Item key="/about"><Link to="/about">About</Link></Menu.Item>
+        <Menu.Item key="/profile"><Link to="/profile">Profile</Link></Menu.Item>
+      </>
+    );
+
+    return (
+      <>
+        <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
+        <Menu.Item key="/register"><Link to="/register">Register</Link></Menu.Item>
+        <Menu.Item key="/login"><Link to="/login">Login</Link></Menu.Item>
+      </>
+    );
+  }
+
+  function renderRoutes() {
+    if (isAuth) return (
+      <Switch>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+        <Route path="/profile">
+          <h1>Profile</h1>
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    );
+  }
+
+  return (
+    <Switch>
+      <Route path="/login">
+        <h1>Login</h1>
+      </Route>
+      <Route path="/register">
+        <h1>Register</h1>
+      </Route>
+      <Route path="/">
+        <h1>Home</h1>
+      </Route>
+    </Switch>
+  )
+}
